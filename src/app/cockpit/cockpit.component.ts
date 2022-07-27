6f674031e6cd5c4b8d967c1e922abae9cf13b3a3
A7Aima <aimaabhilash@gmail.com>
2022-07-27 14:11:44 +0530

    View child implementation

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -12,22 +12,23 @@ export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output() blueprintCreated = new EventEmitter<{ blueprintName: string, blueprintContent: string }>();
   newServerContent = '';
+  @ViewChild("newServerContentData", { static: true }) serverContentInput: ElementRef;
 
   onAddServer(inputName: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: inputName.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
     inputName.value = "";
-    this.newServerContent = '';
+    this.serverContentInput.nativeElement.value = "";
   }
 
   onAddBlueprint(inputName: HTMLInputElement) {
     this.blueprintCreated.emit({
       blueprintName: inputName.value,
-      blueprintContent: this.newServerContent,
+      blueprintContent: this.serverContentInput.nativeElement.value,
     });
     inputName.value = "";
-    this.newServerContent = '';
+    this.serverContentInput.nativeElement.value = "";
   }
 }
