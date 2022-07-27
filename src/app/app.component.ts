eff8efdabceca047cda55b9c5670f47f97626f02
A7Aima <aimaabhilash@gmail.com>
2022-07-27 12:16:28 +0530

    View Encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  // it restricts the value contained inside the components.
+  // (putting ViewEncapsulation.None removes the component encapsulation ).
+  // Default is ViewEncapsulation.Emulated.
+  // ShadowDom will hide the whole logic in the dom
 })
 export class AppComponent {
   serverElements = [{ type: "server", name: "Testserver", content: "Just a test!" }];
